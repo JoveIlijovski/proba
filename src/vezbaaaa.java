2cab8a253a6a43d84df12bbaec9802dc14358c0a
Jove Ilijovski <jove@test.com>
2022-12-16 23:00:43 +0100

    Da se probame

---
+++ src/vezbaaaa.java
@@ -0,0 +1,15 @@
+public class vezbaaaa {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		
+		var x = new vezbaaaa();
+		x.hrana();
+
+	}
+	public void hrana()   {
+		 System.out.println("Hello tacos");
+	}
+
+}
